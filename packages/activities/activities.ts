50a6066ecc200dfe5c39b9c91da0fcd4065cd2cc
Kevin Wu <kevin.wu@mckinsey.com>
2024-02-01 13:34:53 -0500

    - cleaned app - added docker,docker-compose,tilt functionalities - added dev and prod env - all services are able to communicate in local, dev and prov enviroments

---
+++ packages/activities/activities.ts
@@ -0,0 +1,11 @@
+async function otpCheck(otp: string): Promise<boolean> {
+  return otp === '1234';
+}
+
+export async function verifyOTP(otp: string): Promise<boolean> {
+  console.log('IN verifyOTP activity');
+  const otp_status = await otpCheck(otp);
+  console.log('DONE verifyOTP activity: ', otp_status);
+
+  return otp_status;
+}
